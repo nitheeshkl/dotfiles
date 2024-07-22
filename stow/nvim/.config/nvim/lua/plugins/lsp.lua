0da40c5c9ecc3904abc9aaf4e90f96887866cba9
Nitheesh K L <nitheeshkl@gmail.com>
2024-07-22 02:39:41 -0700

    nvim: add none-ls support for lsp

--- stow/nvim/.config/nvim/lua/plugins/lsp.lua
+++ stow/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,35 +2,37 @@ return {
   {
     "williamboman/mason.nvim",
     config = function()
-
       require("mason").setup()
-
-    end
+    end,
   },
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
-
       require("mason-lspconfig").setup({
         ensure_installed = {
           "lua_ls",
           "pyright",
-        }
+        },
       })
-
-    end
+    end,
   },
   {
     "neovim/nvim-lspconfig",
     config = function()
-
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.pyright.setup({})
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
-
-    end
-  }
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({ sources = { null_ls.builtins.formatting.stylua, null_ls.builtins.formatting.black } })
+      vim.keymap.set({ "n", "v" }, "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
 }
--- stow/nvim/.config/nvim/lua/plugins/treesitter.lua
+++ stow/nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,26 +1,50 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = function()
-      require("nvim-treesitter.install").update({ with_sync = true })()
+    require("nvim-treesitter.install").update({ with_sync = true })()
   end,
   config = function()
-
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { 
-        "bash", "c", "csv", "cmake", "cpp", "css", "diff",
-        "dockerfile", "git_config", "git_rebase",
-        "gitattributes", "gitcommit", "gitignore", "html", "json",
-        "javascript", "lua", "luadoc", "make", "markdown",
-        "markdown_inline",  "proto", "python", "rust", "scss",
-        "ssh_config", "tmux","typescript", "vim", "vimdoc", "xml", "yaml",
+      ensure_installed = {
+        "bash",
+        "c",
+        "csv",
+        "cmake",
+        "cpp",
+        "css",
+        "diff",
+        "dockerfile",
+        "git_config",
+        "git_rebase",
+        "gitattributes",
+        "gitcommit",
+        "gitignore",
+        "html",
+        "json",
+        "javascript",
+        "lua",
+        "luadoc",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "proto",
+        "python",
+        "rust",
+        "scss",
+        "ssh_config",
+        "tmux",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
 
         -- "bibtex", "latex",
-
       },
+      auto_install = true,
       highlight = { enable = true },
-      indent = { enable = true },  
+      indent = { enable = true },
     })
-    
-  end
+  end,
 }
