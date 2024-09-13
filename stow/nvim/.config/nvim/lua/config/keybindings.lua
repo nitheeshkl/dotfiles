316465f206c1f17a2c7f5ffb594ceb5bd0c32331
Nitheesh K L <nitheesh@nimble.ai>
2024-09-13 10:54:30 -0700

    update nvim keybindings

--- stow/nvim/.config/nvim/lua/config/keybindings.lua
+++ stow/nvim/.config/nvim/lua/config/keybindings.lua
@@ -4,3 +4,28 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- toggle paste mode
+vim.cmd("nnoremap <C-v> :set invpaste paste?<CR>")
+vim.cmd("set pastetoggle=<C-v>")
+vim.cmd("set showmode")
+
+-- turn off search highlights
+vim.cmd("nnoremap <leader><space> :nohlsearch<CR>")
+
+-- window & splits
+vim.cmd("nnoremap <C-J> <C-W><C-J>")
+vim.cmd("nnoremap <C-K> <C-W><C-K>")
+vim.cmd("nnoremap <C-L> <C-W><C-L>")
+vim.cmd("nnoremap <C-H> <C-W><C-H>")
+
+vim.cmd("nnoremap <silent> <leader>s :split<CR>")
+vim.cmd("nnoremap <silent> <leader>v :vsplit<CR>")
+
+-- tabs key bindings
+vim.cmd("map <C-Up> :tabr<CR>")
+vim.cmd("map <C-Down> :tabl<CR>")
+vim.cmd("map <C-left> :tabp<CR>")
+vim.cmd("map <C-right> :tabn<CR>")
+
+-- clipboard
+vim.keymap.set('v', '<leader>y', '"+y')
