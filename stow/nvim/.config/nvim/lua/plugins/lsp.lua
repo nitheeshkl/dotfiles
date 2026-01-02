9240d984961fed891f1bb71d3ee1e3a59190a198
Nitheesh K L <nitheesh@nimble.ai>
2026-01-01 23:35:53 -0800

    update lspconfig loading

--- stow/nvim/.config/nvim/lua/plugins/lsp.lua
+++ stow/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -25,9 +25,9 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.pyright.setup({})
+      vim.lsp.config("lua_ls", {})
+      vim.lsp.config("pyright", {})
+      vim.lsp.enable({ "lua_ls", "pyright" })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
